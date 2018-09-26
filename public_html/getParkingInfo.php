87df093c8f0ff4cffdf6183863e9786d867d9288
Simphiwe Zulu <30542929+szulu107@users.noreply.github.com>
2018-09-26 17:15:33 +0200

    public_html folder added
    This folder reflects the one used in the server to host the website

---
+++ public_html/getParkingInfo.php
@@ -0,0 +1,15 @@
+<?php
+//Returns the entire parking_lot info database.
+
+
+require_once('../config.php'); //connect to db
+
+$query = "SELECT * from PARKING_LOT" ; //build query
+$result = mysqli_query($dbc,$query);  // run query
+
+$rows = $result->fetch_all(MYSQLI_ASSOC); //fetch all rows
+header('Content-Type: application/json'); 
+echo json_encode($rows);             //encode php array as json
+mysqli_close($dbc);		     //close the connection	
+
+?>	
