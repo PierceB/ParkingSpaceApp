5cebbdeff4b05c3a14d33fd033197c718a546c06
dione <dione@dione.ms.wits.ac.za>
2018-11-08 14:38:07 +0200

    More trying

--- ImageCropper/Details.py
+++ ImageCropper/Details.py
@@ -2,4 +2,4 @@ snapshot = "snapshot.jpeg"       #Name of the picture to use
 hostn = "localhost"              #Name for host for mysql connection
 usern = "connect"                #Name for account to connect to db
 passw = "connectpw"              #password for the above
-dbname = "PARKINGAPPDB"          # database name
+dbname = "APSAPP_DB"          # database name
