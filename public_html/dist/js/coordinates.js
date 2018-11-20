ce4174ce81ee9458b737df7f036e45ea093f0a4b
Simphiwe Zulu <30542929+szulu107@users.noreply.github.com>
2018-11-20 12:31:15 +0200

    Clear and Coordinates function edited
    Coordinates function obtains mouseclick coordinates and displays them on screen. Clear function erases canvas using code from draw.js.

--- public_html/dist/js/coordinates.js
+++ public_html/dist/js/coordinates.js
@@ -1,20 +1,23 @@
 $(document).ready(function () {
-    var coordinates = [];
+
     $("#can").click(function (event) {
         var posX = event.pageX - $(this).offset().left;
         var posY = event.pageY - $(this).offset().top;
-
         /*
         push coordinates to input box
          */
         if ($("#coord1").val() == ""){
             $("#coord1").val(posX + "," + posY);
+            drawDot(posX, posY);
         }else if ($("#coord2").val() == ""){
             $("#coord2").val(posX + "," + posY);
+            drawDot(posX, posY);
         }else if ($("#coord3").val() == ""){
             $("#coord3").val(posX + "," + posY);
+            drawDot(posX, posY);
         } else if ($("#coord4").val() == "") {
             $("#coord4").val(posX + "," + posY);
+            drawDot(posX, posY);
         }else {
             alert("4 points have been clicked already. Please click save or clear.");
         }
@@ -25,5 +28,15 @@ $(document).ready(function () {
         $("#coord2").val("");
         $("#coord3").val("");
         $("#coord4").val("");
+        erase();
     });
 });
+
+function drawDot(x, y) {
+    var context = $('#can').getContext('2d');
+    context.fillStyle = '#000000';
+    context.beginPath();
+    context.arc(x, y, 50, 0, 2*Math.PI);
+    context.fill();
+    context.closePath();
+}
