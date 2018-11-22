f22532168f8eb64c131488906e15fd82f9bda1fe
Simphiwe Zulu <30542929+szulu107@users.noreply.github.com>
2018-11-22 17:46:11 +0200

    More functionality added
    Script now retrieves parking bay data from database and draws selected parking bay on canvas. Script also draws dots on canvas when user clicks on canvas.

--- public_html/dist/js/coordinates.js
+++ public_html/dist/js/coordinates.js
@@ -1,5 +1,35 @@
-$(document).ready(function () {
+var context = null;
+var w, h, img;
+var lineWidth = 2;
+var color = 'red';
 
+$(document).ready(function () {
+    $("#parkingspace").change(function (event) {
+        clear();
+        var coordinates = null;
+        var parkingbay = $('#parkingspace option:selected').val();
+        $.ajax({
+            url: 'http://dione.ms.wits.ac.za/php/getcoords.php',
+            type: 'GET',
+            dataType: 'html',
+            data:{
+                'parkingbay': parkingbay
+            },
+            success: function (data) {
+                coordinates = JSON.parse(data);
+                var coord1 = coordinates.coord1.split(',');
+                var coord2 = coordinates.coord2.split(',');
+                var coord3 = coordinates.coord3.split(',');
+                var coord4 = coordinates.coord4.split(',');
+
+                drawLine(coord1, coord2);
+                drawLine(coord2, coord3);
+                drawLine(coord3, coord4);
+                drawLine(coord4, coord1);
+            }
+        });
+
+    });
     $("#can").click(function (event) {
         var posX = event.pageX - $(this).offset().left;
         var posY = event.pageY - $(this).offset().top;
@@ -32,11 +62,55 @@ $(document).ready(function () {
     });
 });
 
+function init() {
+    context = $('#can')[0].getContext('2d');
+    img = new Image();
+    img.onload = function () {
+        context.drawImage(img,0,0, $('#can')[0].width, $('#can')[0].height);
+    };
+    img.src = '../../images/wits_msl/02.png';
+    w = $('#can')[0].width;
+    h = $('#can')[0].height;
+}
+
+function erase() {
+    var m = confirm("Want to clear");
+    if (m) {
+        context.clearRect(0, 0, w, h);
+        img = new Image();
+        img.onload = function() {
+            context.drawImage(img,0,0, w, h);
+        };
+        img.src = '../../images/wits_msl/02.png';
+    }
+}
+
+function clear() {
+    context.clearRect(0, 0, w, h);
+    img = new Image();
+    img.onload = function() {
+        context.drawImage(img,0,0, w, h);
+    };
+    img.src = '../../images/wits_msl/02.png';
+}
+
 function drawDot(x, y) {
-    var context = $('#can').getContext('2d');
-    context.fillStyle = '#000000';
+    context = $('#can')[0].getContext('2d');
+    context.fillStyle = 'red';
     context.beginPath();
-    context.arc(x, y, 50, 0, 2*Math.PI);
+    context.arc(x, y, 2, 0, 2*Math.PI);
     context.fill();
     context.closePath();
+}
+
+function drawLine(point1, point2) {
+    context = $('#can')[0].getContext('2d');
+    context.beginPath();
+    context.moveTo(point1[0], point1[1]);
+    context.lineTo(point2[0], point2[1]);
+    context.fillStyle = 'red';
+    context.lineWidth = lineWidth;
+    context.stroke();
+    context.closePath();
+
 }
