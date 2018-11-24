9a53139e32953b5b861da775a09d6f1438e753de
Simphiwe Zulu <30542929+szulu107@users.noreply.github.com>
2018-11-24 12:02:21 +0200

    generate.php updated
    Updates IMAGES table when image has been cropped

--- public_html/php/generate.php
+++ public_html/php/generate.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * Created by PhpStorm.
@@ -12,28 +13,42 @@ if (isset($_GET['current']) && isset($_POST['generate'])) {
     $parkinglot = $_GET['current'];
     $message=$parkinglot;
     $filename = $parkinglot . $date;
+    //$parkinglot = 'wits_msl';
     //chmod('/home/dione/ParkingSpaceApp/ImageCropper', 0777);
-    $command = 'python3 /home/dione/ImageCropper/Generate.py ' . $parkinglot . ' ' . $filename;//. $parkinglot;
+    $command = 'python3 /home/dione/ImageCropper/Generate.py ' . $parkinglot . ' ' . $filename. ' 2>&1';//. $parkinglot;
     ob_start();
     passthru($command);
     $output = ob_get_clean();
-    $zipname = "/home/dione/public_html/php/" . $filename . ".zip";
-    $outputtag = 'Output from Image Cropper:<br>' . $output;
+    $zipname =   $filename . ".zip";
+    $outputtag = 'Output from Image Cropper:<br>' . $output. ' <br> ' . $command;
+
+    $query = "select * from IMAGES where LOT_ID = '$parkinglot' and IMAGE_CROPPED=0";
+    $result = mysqli_query($dbc, $query);
+    if ($row = mysqli_fetch_array($result)){
+        $path = $row['IMAGE_NAME'];
+    }else{
+        $path = 'No Images';
+    }
 
     if (file_exists($zipname)) {
 //echo "File exists";
+        $query = "update IMAGES set IMAGE_CROPPED=1 where LOT_ID = '$parkinglot' and IMAGE_NAME='$path'";
+        $result = mysqli_query($dbc, $query);
+        $temp = basename($zipname) ;
         $message = 'File exists';
         header('Content-Description: File Transfer');
         header("Content-Type: application/zip");
-        header('Content-Disposition: attachment; filename="' . basename($zipname) . '"');
+        header('Content-Disposition: attachment; filename='.$temp);
         header('Expires: 0');
         header('Cache-Control: must-revalidate');
         header('Pragma: public');
         header('Content-Length: ' . filesize($zipname));
+        ob_clean();
+        flush();
         readfile($zipname);
+        $message = 'Files generated';
     } else {
-       // $message = 'Failed to generate files';
-
+        $message = 'Failed to generate files';
     }
 }
-
+?>
